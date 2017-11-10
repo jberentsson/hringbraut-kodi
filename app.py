26d268f2a5bc870b2f2902a2f241e42ba4fc5bac
Jóhann Berentsson <jberentsson@gmail.com>
2017-11-10 02:38:45 +0000

    Misc.

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ class Hringbraut:
         self.soup = BeautifulSoup(url, 'html.parser')
 
     def get_shows(self):
+        """ Get the name and urls for the shows. """
+        shows = []
         s = self.soup.find(id="box_sitemap_47").find(id="subnavigation-23")
         for link in s.find_all('a'):
-            l = link.get('href')
-            t = link.get_text()
-            if (l and t) and '/thaettir/' in l:
-                print('%s %s' % (t, l))
+            url = link.get('href')
+            name = link.get_text()
+            if '/thaettir/' in url:
+                shows.append({'name': name, 'url': url})
+                
+        return {'shows': shows}
 
 if __name__ == "__main__":
     h = Hringbraut()
-    h.get_shows()
+    out = h.get_shows()
+    print(out)
