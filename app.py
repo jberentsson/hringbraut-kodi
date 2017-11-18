955bf9eaab56aa3e22dd347b8e92d32a773a7fce
Jóhann Berentsson <jberentsson@gmail.com>
2017-11-18 00:36:07 +0000

    bleh

--- app.py
+++ app.py
@@ -9,7 +9,41 @@ import json
 tv = Hringbraut()
 shows = tv.get_shows()
 
-for show in shows['shows']:
-    item = xbmcgui.ListItem('%s' % show['text'])
-    xbmcplugin.addDirectoryItem(int(sys.argv[1]), '', item, isFolder=0)
-xbmcplugin.endOfDirectory(int(sys.argv[1]))
+addon_handle = int(sys.argv[1])
+
+
+xbmcplugin.setContent(addon_handle, 'episodes')
+
+params = dict(urlparse.parse_qsl(sys.argv[2][1:]))
+action_key = params.get("action_key")
+action_value = params.get("action_value")
+name = params.get("name")
+mode = args.get('mode', None)
+
+
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+def main():
+    # List the shows.
+    for i, show in enumerate(shows['shows']):
+        item = xbmcgui.ListItem('%s' % show['text'])
+        url = build_url({'mode':'show', 'url':show['url']}) 
+        xbmcplugin.addDirectory(addon_handle), url, item, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+def show(url):
+    # List the episodes.
+    episodes = tv.get_episode(url)
+    for e in episodes:
+        item = xbmcgui.ListItem('%s' % show['text'])
+        xbmcplugin.addDirectoryItem(addon_handle, '', item, isFolder=False)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+if mode is None:
+    main()
+
+elif mode[0] = 'show':
+    url=args['url'][0]
+    show(url)
