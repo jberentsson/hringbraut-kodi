8856691044bcf636fff1a57599ea565b0aec5fa6
Jóhann Berentsson <jberentsson@gmail.com>
2018-05-29 04:32:40 +0000

    tests added

---
+++ tests/tests.py
@@ -0,0 +1,16 @@
+import pytest
+	
+def test_other():
+    """
+
+    """
+    assert True == True
+    assert True != False
+
+
+def test_testing():
+    """
+
+    """
+    assert( True != ( False != False))
+    assert( (3*4) == 12 )
